c248ce892f1bdaf496fe1162a987873af71c6c08
zakaria ben salek <zakariaazaraf@gmail.com>
2020-11-24 17:00:43 +0100

    selecting all classes, and declaring classes

---
+++ assets/js/custom/custom.js
@@ -0,0 +1,34 @@
+// Variables
+const cartBtn = document.querySelector('.cart-btn');
+const closeCart = document.querySelector('.close-cart');
+const clearCar = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const productDOM = document.querySelector('.products-center');
+
+// cart
+let cart = [];
+
+// class responsable for getting the data
+class Products{
+
+}
+
+// class responsable for diplaying the data
+class Ui{
+
+}
+
+// class responsable for storing the data
+class Storage{
+
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+
+    console.log("hi from addEventListener !!, Your page was loaded successfully.");
+
+})
