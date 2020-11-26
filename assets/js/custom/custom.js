4e215492a9bd311ec8ffb64e82a2577da3b0c442
zakaria ben salek <zakariaazaraf@gmail.com>
2020-11-26 14:26:07 +0100

    get the data and destructing it, get it ready to desplay

--- assets/js/custom/custom.js
+++ assets/js/custom/custom.js
@@ -9,16 +9,45 @@ const cartTotal = document.querySelector('.cart-total');
 const cartContent = document.querySelector('.cart-content');
 const productDOM = document.querySelector('.products-center');
 
-// cart
+// cart, getting the items from the loca storage
 let cart = [];
 
 // class responsable for getting the data
 class Products{
 
+    // function which get the data
+    async getProducts(){
+
+        try {
+
+            let result = await fetch('./../../../products.json')
+
+            let data = await result.json() // convert data
+
+            // DESTRUCTING DATA
+            let products = data.items;
+            products = products.map(item =>{
+                const {title, price} = item.fields;
+                // const id = item.sys;
+                const {id} = item.sys;
+                const image = item.fields.image.fields.file.url;
+                return {id, title, price, image};
+            });
+
+            return products;
+
+        } catch (error) {
+
+            console.log(error)
+
+        }
+        
+    }
+
 }
 
 // class responsable for diplaying the data
-class Ui{
+class UI{
 
 }
 
@@ -31,4 +60,19 @@ document.addEventListener('DOMContentLoaded',()=>{
 
     console.log("hi from addEventListener !!, Your page was loaded successfully.");
 
-})
+    const ui = new UI();
+    const products = new Products();
+
+    products.getProducts().then(products =>{
+
+        console.log(products)
+
+    }).catch(err =>{
+
+        console.log(err)
+
+    })
+
+    
+
+});
