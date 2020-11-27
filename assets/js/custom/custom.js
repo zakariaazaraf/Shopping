bdcd078df2645af803ae0dfb31daaec26a592332
zakaria ben salek <zakariaazaraf@gmail.com>
2020-11-28 00:15:09 +0100

    move on in the project

--- assets/js/custom/custom.js
+++ assets/js/custom/custom.js
@@ -12,6 +12,8 @@ const productDOM = document.querySelector('.products-center');
 // cart, getting the items from the loca storage
 let cart = [];
 
+let buttonsDOM = [];
+
 // class responsable for getting the data
 class Products{
 
@@ -66,35 +68,153 @@ class UI{
                         </button>
                     </div>
                     <h3>${product.title}</h3>
-                    <h4>${product.price}</h4>
+                    <h4>$${product.price}</h4>
                 </article>
                 <!-- End Single Product Example-->
             `;
         });
 
-        // select the producs center and append a child inside it
-        document.querySelector('.products-center').innerHTML = result;
+        // insert products inside producr-center section
+        productDOM.innerHTML = result;
         
+    } 
+
+    
+
+    // Start bagButtons function, handel the cart and buttons behavior
+    getBagButtons(){
+
+        // i used spread operator, returned all buttons, you can return an array [...buttons]
+        const buttons = [...document.querySelectorAll('.bag-btn')];
+
+        buttonsDOM = buttons; // assign those buttons to the parent buttons's holders
+
+        buttons.forEach(button => {
+            let id = button.dataset.id; // OR button.getAttribute('data-id')
+            // To chack if the product i cart => make some changes on buttons
+            const inCart = cart.find(item => item.id === id);
+
+            if(inCart){
+                button.innerText = 'In Cart';
+                button.disabled = true;
+            }
+
+            button.addEventListener('click', e =>{
+
+                e.target.innerText = 'In Cart';
+                e.target.disabled = true;
+                
+                // get the product from the localstorage
+                let cartItem = {...Storage.getProduct(e.target.dataset.id), amount: 1}; // destruct the data and added amount prop
+                
+                // add product to the cart
+                cart = [...cart, cartItem];
+                console.log(cart)
+                //Storage.addProductToCart(cartItem);
+
+                // save cart in local storage
+                Storage.saveCart(cart); // pass the array of cart items to stor in local sotage
+
+                // set cart values
+                this.setCartValues(cart);
+                // display cart values
+
+                // show the cart
+                
+                
+                
+            });
+            
+        })
+            
     }
+
+    // setting the cart values function
+    setCartValues(cart){
+
+        let itemsTotal = 0, // You Sould Initialize Them To Not Get 'Nan'
+            totalPrice = 0;
+        cart.map(item => {
+            itemsTotal += item.amount;
+            totalPrice += item.price * item.amount;
+
+            console.log(item.amount);
+            console.log(item.price);
+        });
+
+        cartItems.innerText = itemsTotal;
+        cartTotal.innerText = parseFloat(totalPrice.toFixed(2));
+
+    }
+
+    
 }
 
 // class responsable for storing the data
 class Storage{
 
+    // save products in the localstorage
+    static saveProducts(products){
+        localStorage.setItem('products', JSON.stringify(products));
+    }
+
+    // get a specific product from the local storage
+    static getProduct(id){
+        const products = JSON.parse(localStorage.getItem('products'));
+        return products.find(product => product.id === id);
+    }
+
+    // save cart items in local storage
+    static saveCart(cart){
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }
+
+    /* static addProductToCart(product){
+        cartContent.innerHTML = `
+            <!-- Start Item -->
+                <div class="cart-item">
+                    <img src="${product.image}" alt="CartProduct">
+                    <div>
+                        <h4>${product.title}</h4>
+                        <h5>${product.price}$</h5>
+                        <span class="remove-item">remove</span>
+                    </div>
+                    <div>
+                        <i class="fas fa-chevron-up"></i>
+                        <p class="item-amount">1</p>
+                        <i class="fas fa-chevron-down"></i>
+                    </div>
+                </div>         
+                <!-- End Item -->
+        `;
+    } */
+
 }
 
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded',() => {
 
-    console.log("hi from addEventListener !!, Your page was loaded successfully.");
+    
 
     const ui = new UI();
     const products = new Products();
 
-    products.getProducts().then(products => ui.displayProducts(products)).catch(err =>{
+    products.getProducts().then(products => {
+
+        ui.displayProducts(products);
+
+        // Is the function is static one you don't have to make an instance of it to work with
+        Storage.saveProducts(products);
+
+    }).then(() =>{
+
+        ui.getBagButtons();
+        
+
+    }).catch(err => {
 
         console.log(err)
 
-    })
+    });
 
     
 
