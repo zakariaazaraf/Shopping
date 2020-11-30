497a4e1de0edacdf41856977cc4a9b7f4097610d
zakaria ben salek <zakariaazaraf@gmail.com>
2020-11-30 21:51:13 +0100

    finally complete the project, well done to myself

--- assets/js/custom/custom.js
+++ assets/js/custom/custom.js
@@ -1,3 +1,15 @@
+//const contentful = require("contentful");
+const client = contentful.createClient({
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: "8we6hnqr6vgm",
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken: "SGiqc1L9egbWiK7oh931DI5MGQvnjIdk7VtQbLa9hrU"
+});
+
+
+
+
+
 // Variables
 const cartBtn = document.querySelector('.cart-btn');
 const closeCart = document.querySelector('.close-cart');
@@ -22,12 +34,20 @@ class Products{
 
         try {
 
+            // data coming from the contentful
+            let contentful = await client.getEntries({
+                content_type: 'productProjet'
+            });
+            console.log(contentful.items);
+
             let result = await fetch('./../../../products.json')
 
             let data = await result.json() // convert data
 
             // DESTRUCTING DATA
-            let products = data.items;
+            //let products = contentful.items;
+            let products = [...data.items, ...contentful.items]; // combin two source of data, local one and the contentful one
+            //let products = data.items;
             products = products.map(item =>{
                 const {title, price} = item.fields;
                 // const id = item.sys;
